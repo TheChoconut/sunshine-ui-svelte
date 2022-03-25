eeac0595f618ae9a22e9a570a34e8dd5794fb2b9
TheChoconut <thechocomail@gmail.com>
2022-03-25 21:57:22 +0100

    update packages

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,5 +1,20 @@
 module.exports = {
   mode: 'jit',
   purge: ['./src/**/*.{html,js,svelte,ts}'],
-  darkMode: 'class'
+  darkMode: 'class',
+  theme: {
+    extend: {
+      colors: {
+        accent: {
+          100: '#98ECFE',
+          200: '#60CCFE',
+          300: '#0093F9',
+          500: '#0078D4',
+          700: '#005EB7',
+          800: '#003D92',
+          900: '#001968'
+        }
+      }
+    }
+  }
 }
